e17d330c54a1bcf7264ef9e5d614a4398e80bac9
chenxuanzzy <chenxsuan019@gmail.com>
2022-02-16 11:15:11 +0800

    🎉  [feat] init a new project

---
+++ Algorithms/Easy/Merge Two Sorted Lists/index.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+var mergeTwoLists = function (l1, l2) {
+  let newList = new ListNode(0);
+  let head = newList;
+
+  while (l1 != null && l2 != null) {
+    if (l1.val < l2.val) {
+      newList.next = l1;
+      l1 = l1.next;
+    } else {
+      newList.next = l2;
+      l2 = l2.next;
+    }
+    newList = newList.next;
+  }
+
+  if (l1 == null) {
+    newList.next = l2;
+  } else {
+    newList.next = l1;
+  }
+  return head.next;
+};
---
+++ Algorithms/Easy/Roman to Integer/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var romanToInt = function (s) {
+  let symbols = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let sum = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (symbols[s[i]] < symbols[s[i + 1]]) {
+      sum -= symbols[s[i]];
+    } else {
+      sum += symbols[s[i]];
+    }
+  }
+  return sum;
+};
