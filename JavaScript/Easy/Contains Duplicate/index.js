cbcdca3639dd07d0ecbf34b6f605537aa49064ae
chenxuanzzy <chenxsuan019@gmail.com>
2022-10-14 15:01:19 +0800

    Revert "revise limit"
    This reverts commit 99278aaf294a3551a302ba2338191761aeb731b7.

--- JavaScript/Easy/Contains Duplicate/index.js
+++ JavaScript/Easy/Contains Duplicate/index.js
@@ -6,13 +6,14 @@
 //Presorting Solution
 var containsDuplicate = function (nums) {
   nums.sort((a, b) => a - b);
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < num.length - 1; i++) {
     if (nums[i] === nums[i + 1]) {
       return true;
     }
   }
   return false;
 };
+
 //Time Complexity: O(n log n)
 //Space Complexity: O(log n)
 
